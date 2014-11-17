8941f051f537b1078eb1c993d5dcca57e5faa391
oharato <oharato@live.jp>
2014-11-18 00:28:20 +0900

    input

---
+++ lib/patlite/input.rb
@@ -0,0 +1,21 @@
+require 'wiringpi2'
+
+module Patlite
+  class Input
+    input_gpio = 2
+
+    def self.wait_input
+      io = WiringPi::GPIO.new
+# http://www.rubydoc.info/gems/wiringpi2/2.0.1/WiringPi/GPIO#pull_up_dn_control-instance_method
+      io.pin_mode(input_gpio, WiringPi::INPUT)}
+      io.pull_up_dn_control(input_gpio, WiringPi::PUD_DOWN)}
+
+      while io.read(4) == WiringPi::LOW
+      end
+
+      $is_flash = false
+
+    end
+
+  end
+end
