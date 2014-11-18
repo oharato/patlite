9c895038b1bde3c199d4c0ce3ef7a7b5fdf3dce9
oharato <oharato@live.jp>
2014-11-18 22:29:01 +0900

    input test

---
+++ lib/patlite/input_test.rb
@@ -0,0 +1,13 @@
+require 'wiringpi2'
+
+input_gpio = 2
+
+io = WiringPi::GPIO.new
+# http://www.rubydoc.info/gems/wiringpi2/2.0.1/WiringPi/GPIO#pull_up_dn_control-instance_method
+io.pin_mode(input_gpio, WiringPi::INPUT)
+io.pull_up_dn_control(input_gpio, WiringPi::PUD_DOWN)
+
+while true
+  p 'hoge' if io.read(input_gpio) == WiringPi::LOW
+  sleep 0.1
+end
