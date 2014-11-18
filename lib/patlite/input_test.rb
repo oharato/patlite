d4dca0d23ac41431c423ce186e0a8a50738c60f5
oharato <oharato@live.jp>
2014-11-18 22:37:01 +0900

    input test

--- lib/patlite/input_test.rb
+++ lib/patlite/input_test.rb
@@ -5,7 +5,7 @@ input_gpio = 2
 io = WiringPi::GPIO.new
 # http://www.rubydoc.info/gems/wiringpi2/2.0.1/WiringPi/GPIO#pull_up_dn_control-instance_method
 io.pin_mode(input_gpio, WiringPi::INPUT)
-io.pull_up_dn_control(input_gpio, WiringPi::PUD_DOWN)
+io.pull_up_dn_control(input_gpio, WiringPi::PUD_UP)
 
 while true
   p 'hoge' if io.digital_read(input_gpio) == WiringPi::LOW
