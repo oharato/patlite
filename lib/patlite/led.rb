12d6c37e4530ba2c9db53fe7de6b5aca7c8ea9a1
oharato <oharato@live.jp>
2014-11-29 22:32:04 +0900

    leds

--- lib/patlite/led.rb
+++ lib/patlite/led.rb
@@ -91,7 +91,8 @@ module Patlite
       @leds.each do |led|
         led.each{|gpio| io.soft_pwm_create(gpio, 0, 255)}
       end
-      [0.01,0.05,0.1,0.5].each do |t|
+      [0.01,0.05,0.1].each do |t|
+        p t
         10.times do
           @leds.each do |led|
             color_name_to_flash(io, led, 'Red')
